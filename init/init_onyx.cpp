ee84464be3c6894e75040f9e62445b384a4b5fd6
Shahab Khan <37383411+Shahabkhan99@users.noreply.github.com>
2019-04-17 05:54:20 +0300

    Add files via upload

---
+++ init/init_onyx.cpp
@@ -0,0 +1,90 @@
+/*
+   Copyright (c) 2016, The CyanogenMod Project
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include <android-base/properties.h>
+#include <android-base/logging.h>
+
+#include "property_service.h"
+#include "vendor_init.h"
+
+using android::base::GetProperty;
+using android::init::property_set;
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
+void vendor_load_properties()
+{
+    std::string platform, rf_version, device;
+
+    platform = GetProperty("ro.board.platform", "");
+    if (platform != ANDROID_TARGET)
+        return;
+
+    rf_version = GetProperty("ro.boot.rf_version", "");
+
+    // Init a dummy BT MAC address, will be overwritten later
+    property_set("ro.boot.btmacaddr", "00:00:00:00:00:00");
+
+    if (rf_version == "101") {
+        /* China */
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "ONE E1001");
+        property_set("ro.rf_version", "TDD_FDD_Ch_All");
+    } else if (rf_version == "102") {
+        /* Asia/Europe */
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "ONE E1003");
+        property_set("ro.rf_version", "TDD_FDD_Eu");
+    } else if (rf_version == "103"){
+        /* America */
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "ONE E1005");
+        property_set("ro.rf_version", "TDD_FDD_Am");
+    } else if (rf_version == "107"){
+        /* China CTCC Version */
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "ONE E1000");
+        property_set("ro.rf_version", "TDD_FDD_ALL_OPTR");
+    }
+    device = GetProperty("ro.product.device", "");
+    LOG(INFO) << "Found rf_version : " << rf_version.c_str() << " setting build properties for " << device.c_str() << " device\n";
+}
---
+++ libshims/Android.mk
@@ -0,0 +1,39 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := sensors.cpp
+LOCAL_SHARED_LIBRARIES := libgui libsensor
+LOCAL_MODULE := libshims_sensors
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := atomic.cpp
+LOCAL_SHARED_LIBRARIES := libcutils
+LOCAL_MODULE := libshims_atomic
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := camera.cpp
+LOCAL_SHARED_LIBRARIES := libui
+LOCAL_MODULE := libshims_camera
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := CameraParameters.cpp
+LOCAL_MODULE := libshims_camera_parameters
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
